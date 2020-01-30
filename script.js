f03e78a10f0b3df6ad4b754fe66ca744281f8aa7
Tosha Shigenina <shigenina@gmail.com>
2020-01-30 22:50:39 +0500

    Написана функция бота, исправлена опечатка в html

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 'use strict';
 
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
-function randomInteger() {
+function randomBot() {
   let min = 1,
     max = 100;
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
-  return Math.round(rand);
+
+  function randomInt() {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
+
+  let random = randomInt();
+  console.log(random);
+
+  function callUser() {
+    let numUser = prompt('Угадай число от 1 до 100');
+
+    if (numUser !== null) {
+      if (isNumber(numUser)) {
+        if (Number(numUser) === random) {
+          alert('Верно!');
+        } else {
+          if (numUser > random) {
+            alert('Загаданное число меньше');
+            callUser();
+          } else {
+            alert('Загаданное число больше');
+            callUser();
+          }
+        }
+      } else {
+        alert('Введи число!');
+        callUser();
+      }
+    }
+  }
+
+  callUser();
 }
+
+randomBot();
