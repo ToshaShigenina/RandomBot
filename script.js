f3d10f74c17a6700f3d6e5f7bc999a2c23d2a2a3
Tosha Shigenina <shigenina@gmail.com>
2020-01-30 23:08:57 +0500

    Добавлен усложненный функционал

--- script.js
+++ script.js
@@ -13,27 +13,47 @@ function randomBot() {
     return Math.round(rand);
   }
 
-  let random = randomInt();
+  let steps = 9,
+    random = randomInt();
   console.log(random);
 
   function callUser() {
-    let numUser = prompt('Угадай число от 1 до 100');
 
-    if (numUser !== null) {
+    let numUser = prompt('Угадай число от 1 до 100');
+    let choice;
+
+    if (numUser !== null && steps > 0) {
       if (isNumber(numUser)) {
         if (Number(numUser) === random) {
-          alert('Верно!');
+
+          choice = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+
+          if (choice) {
+            steps = 9;
+            random = randomInt();
+            callUser();
+          }
         } else {
           if (numUser > random) {
-            alert('Загаданное число меньше');
+            alert('Загаданное число меньше, осталось попыток ' + steps);
+            --steps;
             callUser();
           } else {
-            alert('Загаданное число больше');
+            alert('Загаданное число больше, осталось попыток ' + steps);
+            --steps;
             callUser();
           }
         }
       } else {
         alert('Введи число!');
+        callUser();
+      }
+    } else if (steps === 0) {
+      choice = confirm('Жаль, Вы не угадали(... Хотели бы сыграть еще?');
+
+      if (choice) {
+        random = randomInt();
+        steps = 9;
         callUser();
       }
     }
